2d6533b69294a63d009d4ec17c2d508a11a32fae
IhorKorotenko <ihor.korotenko@n-cube.co.uk>
2016-09-29 19:45:45 +0300

    rakamakafo fristaylo , Yo

--- app.rb
+++ app.rb
@@ -39,6 +39,12 @@ class HangpersonApp < Sinatra::Base
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
+    begin
+      flash[:message] = 'You have already used that letter.' if !@game.guess(letter)
+    rescue ArgumentError
+      flash[:message] = 'Invalid guess.'
+    end
+
     ### YOUR CODE HERE ###
     redirect '/show'
   end
@@ -50,16 +56,22 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
+    redirect '/win' if @game.check_win_or_lose==:win
+    redirect '/lose' if @game.check_win_or_lose==:lose
     erb :show # You may change/remove this line
   end
   
   get '/win' do
     ### YOUR CODE HERE ###
+    redirect '/show' if @game.check_win_or_lose!=:win
+    redirect '/new' if @game.word.empty?
     erb :win # You may change/remove this line
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
+    redirect '/new' if @game.word.empty?
+    redirect '/show' if @game.check_win_or_lose!=:lose
     erb :lose # You may change/remove this line
   end
   
--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,9 +7,40 @@ class HangpersonGame
 
   # def initialize()
   # end
-  
+
+  attr_accessor :word, :wrong_guesses, :guesses
+
   def initialize(word)
     @word = word
+    @guesses=''
+    @wrong_guesses=''
+  end
+
+  def guess(char)
+    if char.nil?||char.empty?||(char=~/[a-z]/i).nil?
+      raise ArgumentError
+    end
+    char=char.downcase
+    if @wrong_guesses.include?(char)||@guesses.include?(char)
+      return false
+    end
+    if @word.include? char
+      @guesses+=char
+    else
+      @wrong_guesses+=char
+    end
+    true
+
+  end
+
+  def word_with_guesses
+    @word.chars.map { |c| @guesses.include?(c)? c : '-'}.join('')
+  end
+
+  def check_win_or_lose
+    return :lose if @wrong_guesses.length>=7
+    return :win if !@guesses.empty?&&@word==word_with_guesses
+    :play
   end
 
   def self.get_random_word
